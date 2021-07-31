04284176f711e5ecf4234a7b39055ce154d1971d
Stephen Nicholas <stephennicholas.191200@gmail.com>
2021-08-01 02:22:34 +0700

    Added User Interface for Item Chat and Item Forum

---
+++ mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/forum/ForumAdapter.kt
@@ -0,0 +1,26 @@
+package com.bangkitcapstone.coral_id.ui.forum
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ForumAdapter : RecyclerView.Adapter<ForumAdapter.ListHeroAdapter.ListViewHolder>() {
+    inner class ListHeroAdapter(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListHeroAdapter.ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListHeroAdapter.ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/forum/ForumFragment.kt
+++ mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/forum/ForumFragment.kt
@@ -7,7 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bangkitcapstone.coral_id.databinding.FragmentForumBinding
+import com.bangkitcapstone.coral_id.ui.result.ResultAdapter
+import com.loopj.android.http.*
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
 
 class ForumFragment : Fragment() {
 
@@ -27,12 +32,40 @@ class ForumFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val sharedPreferences = activity?.getSharedPreferences("tempBangkit", Context.MODE_PRIVATE) ?: return
-        Log.d("SharedPreferences",sharedPreferences.toString())
-        val email = sharedPreferences.getString("emailAddress", null).toString()
-        val password = sharedPreferences.getString("password", null).toString()
-        Log.d("email", email)
-        Log.d("password", password)
-        binding?.testString?.text = "$email, $password"
+
+//        val email = sharedPreferences.getString("emailAddress", null).toString()
+//        val password = sharedPreferences.getString("password", null).toString()
+        setRecyclerView()
+
+        val client = AsyncHttpClient()
+        val requestParams = RequestParams()
+        client.get("https://api.sampleapis.com/avatar/info", requestParams, object : AsyncHttpResponseHandler(){
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray
+            ) {
+                val response = String(responseBody)
+                Log.d("message", response)
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                Log.d("error", error.toString())
+            }
+
+        })
+    }
+
+    private fun setRecyclerView() {
+        binding?.aCRViewMessages?.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ForumAdapter()
+        }
     }
 
     override fun onDestroy() {
