9bbf4c8c067fda365a110cc02e9677bdb76f65be
Stephen Nicholas <stephennicholas.191200@gmail.com>
2021-08-28 01:34:37 +0700

    Added login and register feature

--- mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/login/LoginFragment.kt
+++ mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/login/LoginFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.bangkitcapstone.coral_id.BuildConfig.BASE_URL
 import com.bangkitcapstone.coral_id.R
@@ -18,6 +19,7 @@ import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import com.loopj.android.http.RequestParams
 import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
 
 class LoginFragment : Fragment() {
 
@@ -48,14 +50,11 @@ class LoginFragment : Fragment() {
             val email = binding?.email?.text.toString()
             val password = binding?.password?.text.toString()
 
-//            editor?.putString("emailAddress", email)
-//            editor?.putString("password", password)
-//            editor?.apply()
             val client = AsyncHttpClient()
             val requestParams = RequestParams()
             requestParams.add("email", email)
             requestParams.add("password", password)
-            client.post(BASE_URL + "login", requestParams, object : AsyncHttpResponseHandler(){
+            client.post("https://86c0-116-206-12-59.ngrok.io/api/auth/login/", requestParams, object : AsyncHttpResponseHandler(){
                 override fun onSuccess(
                     statusCode: Int,
                     headers: Array<out Header>?,
@@ -63,7 +62,15 @@ class LoginFragment : Fragment() {
                 ) {
                     val response = String(responseBody)
                     Log.d("message", response)
-                    it.findNavController().navigate(R.id.action_loginFragment_to_forumFragment)
+
+                    val response_data = JSONObject(response)
+                    val refresh_token = response_data.getString("refresh_token").toString()
+                    val access_token = response_data.getString("access_token").toString()
+                    val message = response_data.getString("message").toString()
+
+                    Log.d("message_from_response", message)
+                    Toast.makeText(getContext(),message,Toast.LENGTH_SHORT).show()
+//                    it.findNavController().navigate(R.id.action_loginFragment_to_forumFragment)
                 }
 
                 override fun onFailure(
@@ -73,9 +80,11 @@ class LoginFragment : Fragment() {
                     error: Throwable?
                 ) {
                     Log.d("error", error.toString())
+                    Toast.makeText(getContext(), error.toString(), Toast.LENGTH_SHORT).show()
                 }
 
             })
+
         }
     }
 
--- mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/register/RegisterFragment.kt
+++ mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/register/RegisterFragment.kt
@@ -2,13 +2,21 @@ package com.bangkitcapstone.coral_id.ui.register
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.bangkitcapstone.coral_id.BuildConfig
 import com.bangkitcapstone.coral_id.R
 import com.bangkitcapstone.coral_id.databinding.FragmentRegisterBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
 
 class RegisterFragment : Fragment() {
 
@@ -35,16 +43,54 @@ class RegisterFragment : Fragment() {
         }
 
         binding?.btnRegister?.setOnClickListener {
+            val name = binding?.name?.text.toString()
             val email = binding?.email?.text.toString()
             val password = binding?.password?.text.toString()
             val confirm_password = binding?.confirmPassword?.text.toString()
 
             if (password == confirm_password){
+                editor?.putString("name", name)
                 editor?.putString("emailAddress", email)
                 editor?.putString("password", password)
                 editor?.apply()
 
-                it.findNavController().navigate(R.id.action_registerFragment_to_forumFragment)
+                val client = AsyncHttpClient()
+                val requestParams = RequestParams()
+
+                requestParams.add("name", name)
+                requestParams.add("email", email)
+                requestParams.add("password", password)
+
+                client.post("https://86c0-116-206-12-59.ngrok.io/api/auth/register/", requestParams, object : AsyncHttpResponseHandler(){
+                    override fun onSuccess(
+                        statusCode: Int,
+                        headers: Array<out Header>?,
+                        responseBody: ByteArray
+                    ) {
+                        val response = String(responseBody)
+                        Log.d("message", response)
+
+                        val response_data = JSONObject(response)
+                        val refresh_token = response_data.getString("refresh_token").toString()
+                        val access_token = response_data.getString("access_token").toString()
+                        val message = response_data.getString("message").toString()
+
+                        Log.d("message_from_response", message)
+                        Toast.makeText(getContext(),message, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Array<out Header>?,
+                        responseBody: ByteArray?,
+                        error: Throwable?
+                    ) {
+                        Log.d("error", error.toString())
+                        Toast.makeText(getContext(), "Error:" + error.toString(), Toast.LENGTH_SHORT).show()
+                    }
+
+                })
+//                it.findNavController().navigate(R.id.action_registerFragment_to_forumFragment)
             }
         }
     }
